eff78ef82c9c013bbe496585556b5aef091d9dcb
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+uadaynalabothu@users.noreply.github.com>
2022-09-10 22:51:15 +0530

    Add files via upload

---
+++ 03_doc_string_functions.py
@@ -0,0 +1,21 @@
+def gm(): 
+  '''Author: Python 12PM
+      The time is now 1pm
+         We are doing functions topic'''
+print(gm.__doc__)
+
+
+
+def gm():
+  """uday rama koteswa rao is a
+        student at lenora college of engineering
+            from 2020 to 2024 
+              studed b.tech("ECE") Course in ours college"""
+print(gm.__doc__)
+
+
+def gm():
+  """Nalabothu Uday Rama Koteswa Rao S/o of Nalabothu Hanumantha Rao
+        6-404, Near Water Tank, Anjanapuram,
+            Gurazala, Guntur, Pincode - 522415"""
+print(gm.__doc__)
