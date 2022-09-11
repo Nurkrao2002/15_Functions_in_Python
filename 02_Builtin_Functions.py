5f8ba1b1027658a21138e10498cd71da81114a8c
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+uadaynalabothu@users.noreply.github.com>
2022-09-11 20:35:04 +0530

    Update 02_Builtin_Functions.py

--- 02_Builtin_Functions.py
+++ 02_Builtin_Functions.py
@@ -14,65 +14,65 @@ print(chr(90))
 
 '''  all()  '''
 
-# print(all([True,1,2]))   
-# print(all([True,'',1,2]))   
-# print(all([True,' ',1,2]))   
-# print(all([True,False,1,2])) 
-# print(all([True,True,1,2,0])) 
-# print(all([True,True,1,2,None])) 
-# print(all((True,True,1,2,None))) 
+print(all([True,1,2]))   
+print(all([True,'',1,2]))   
+print(all([True,' ',1,2]))   
+print(all([True,False,1,2])) 
+print(all([True,True,1,2,0])) 
+print(all([True,True,1,2,None])) 
+print(all((True,True,1,2,None))) 
 
 '''  any() '''
 
-# print(any([True,False,False,0,23])) 
-# print(any([False,False,'']))     
-# print(any([True,True,None,0]))  
+print(any([True,False,False,0,23])) 
+print(any([False,False,'']))     
+print(any([True,True,None,0]))  
 
 '''   bool()  '''
 
-# print(bool(False))
-# print(bool(1))
-# print(bool(0))
-# print(bool(None))
-# print(bool('bool'))
-# print(bool(''))
-# print(bool(' '))
+print(bool(False))
+print(bool(1))
+print(bool(0))
+print(bool(None))
+print(bool('bool'))
+print(bool(''))
+print(bool(' '))
 
 
 '''  eval() '''
 
-# print('eval=',eval("5+6.8-1"))
-# a=eval('5+6-1')
-# b=eval('4+3.8-1')
-# print(a,type(a))
-# print(b,type(b))
+print('eval=',eval("5+6.8-1"))
+a=eval('5+6-1')
+b=eval('4+3.8-1')
+print(a,type(a))
+print(b,type(b))
 
 '''  sum() '''
 
-# print('sum=',sum([1,2,3,4,5]))
-# print('sum=',sum((10.5,20,30)))
+print('sum=',sum([1,2,3,4,5]))
+print('sum=',sum((10.5,20,30)))
 
 
 '''  reversed()-list  '''
 
-# for i in reversed([1,2,3,4]):
-#     print (i)
+for i in reversed([1,2,3,4]):
+    print (i)
 
 '''  reversed()-tuple  '''
 
-# for i in reversed((10,20,30,40)):
-#     print (i)
+for i in reversed((10,20,30,40)):
+    print (i)
 
 '''  enumerate()  '''
 
-# a=['bread','milk','python']
-# b=enumerate(a)
-# print(type(b))
-# print(dict(b))
-# print(list(b))
-# print(tuple(b))
+a=['bread','milk','python']
+b=enumerate(a)
+print(type(b))
+print(dict(b))
+print(list(b))
+print(tuple(b))
 
 
-# a=['bread','milk','python']
-# c=enumerate(a,-1)
-# print(list(c))
+a=['bread','milk','python']
+c=enumerate(a,-1)
+print(list(c))
