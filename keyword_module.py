e93d1612d43a6067c3f119923aa7c1789ce3eff6
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+uadaynalabothu@users.noreply.github.com>
2022-09-12 11:04:38 +0530

    Add files via upload

---
+++ keyword_module.py
@@ -0,0 +1,20 @@
+# import keyword
+# print(keyword.kwlist)
+# print(len(keyword.kwlist))
+# print(len(['False', 'None', 'True', 'and', 'as', 'assert', 'async', 
+# 'await', 'break', 'class','continue','def', 'del', 'elif', 'else',
+# 'except', 'finally', 'for', 'from','global', 'if','import', 'in', 'is',
+# 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try',
+# 'while', 'with', 'yield']))
+# print(keyword.iskeyword('False'))
+# print(keyword.iskeyword('True'))
+# print(keyword.iskeyword('return'))
+# print(keyword.iskeyword('print'))
+# print(keyword.iskeyword('assert'))
+# print(keyword.iskeyword('try'))
+
+
+""" We cannot use keywords as variables or identifiers """
+# None=4
+# True=5
+# while=6
---
+++ zip.py
@@ -0,0 +1,19 @@
+# n1=[10,20,30,40,50,60]
+# n2=[1,2,3,4,5]
+# print(n1+n2)
+# rp=list(zip(n1,n2))
+# print(rp)
+
+# n1=[10,20,30,40,50,60]
+# n2=[1,2,3,4,5]
+# for x,y in zip(n1,n2):
+#     print(x,y)
+
+# n1=[10,20,30,40,50]
+# n2=[1,2,3,4,5]
+# for x,y in zip(n1,n2):
+#     print(x+y,x-y)
+
+# a=range(10,15)
+# b=range(1,5)
+# for x,y in zip(a,b):print(x+y)
