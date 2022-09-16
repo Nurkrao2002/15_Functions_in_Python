9a4dae4ae95d84cd55a7db6cc4bc05f392a06e8c
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+Nurkrao2002@users.noreply.github.com>
2022-09-16 18:05:37 +0530

    Update 11_Lambda_Filter.py

--- 11_Lambda_Filter.py
+++ 11_Lambda_Filter.py
@@ -1,5 +1,5 @@
 # a lambda function that returns even numbers from a list
-# a = [2,7,4,1,8,13,16,19,21,24]
+a = [2,7,4,1,8,13,16,19,21,24]
 a=[int(x) for x in input().split()]
 b = list(filter(lambda x: (x%2 == 0), a))
 c = list(filter(lambda x: (x%2 != 0), a))
